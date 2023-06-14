62a1f1eac7b82d6c1b46f9251b355f029426a774
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-06-14 09:16:14 +0000

    main

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,5 +14,25 @@ class Reservation < ApplicationRecord
     reservation_data
   end
 
+  class Reservation < ApplicationRecord
+  # 略
+
+  validate :date_before_start
+  validate :date_current_today
+  validate :date_three_month_end
+
+  def date_before_start
+    errors.add(:day, "は過去の日付は選択できません") if day < Date.current
+  end
+
+　def date_current_today
+    errors.add(:day, "は当日は選択できません。予約画面から正しい日付を選択してください。") if day < (Date.current + 1)
+  end
+
+  def date_three_month_end
+    errors.add(:day, "は3ヶ月以降の日付は選択できません") if (Date.current >> 3) < day
+  end
+
+
   belongs_to :customer
 end
